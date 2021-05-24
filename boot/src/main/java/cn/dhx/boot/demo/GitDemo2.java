9fcd6d4f76a1886c243c1a65383ef7b715969300
daihongxin <daihongxin@wilcom.com.cn>
2021-05-24 17:23:55 +0800

    git reset

---
+++ boot/src/main/java/cn/dhx/boot/demo/GitDemo2.java
@@ -0,0 +1,17 @@
+package cn.dhx.boot.demo;
+
+public class GitDemo2 {
+
+
+    public static void main(String[] args) {
+        String code="-1";
+        switch (code) {
+            case "-1":
+                System.out.println("---");
+                break;
+            case "1":
+                System.out.println("111");
+
+        }
+    }
+}
