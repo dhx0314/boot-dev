b8b1e4345a23844c8abcce2d2dbb076bcad845ee
dhx <329564635@qq.com>
2021-05-31 20:42:39 +0800

    jvm

---
+++ jvm/src/main/java/cn/dhx/jvm/a2_6/DirectOutOf.java
@@ -0,0 +1,28 @@
+package cn.dhx.jvm.a2_6;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+
+// java.lang.OutOfMemoryError: Direct buffer memory
+
+public class DirectOutOf {
+
+    static int _100Mb = 1024 * 1024 * 100;
+
+    public static void main(String[] args) {
+        ArrayList<ByteBuffer> byteBuffers = new ArrayList<>();
+        int i = 0;
+        try {
+            while (true) {
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(_100Mb);
+                byteBuffers.add(byteBuffer);
+                i++;
+            }
+        } finally {
+            System.out.println(i);
+        }
+
+//        System.gc();
+    }
+}
