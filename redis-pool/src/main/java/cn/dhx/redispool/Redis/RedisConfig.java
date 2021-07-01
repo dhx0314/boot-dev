34b09bb490ff3ceff0a547e7cceb61ebab2e767e
daihongxin <daihongxin@wilcom.com.cn>
2021-07-01 14:54:09 +0800

    redis

--- redis-pool/src/main/java/cn/dhx/redispool/Redis/RedisConfig.java
+++ redis-pool/src/main/java/cn/dhx/redispool/Redis/RedisConfig.java
@@ -1,45 +1,45 @@
-//package cn.dhx.redispool.Redis;
-//
-//import com.fasterxml.jackson.annotation.JsonAutoDetect;
-//import com.fasterxml.jackson.annotation.JsonTypeInfo;
-//import com.fasterxml.jackson.annotation.PropertyAccessor;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//public class RedisConfig {
-//    @Bean
-//    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
-//
-//        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-//        template.setConnectionFactory(factory);
-//
-//        //Json序列化配置
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+package cn.dhx.redispool.Redis;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+
+        //Json序列化配置
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 //        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//
-//        //String序列号配置
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//
-//        //key和hash的key都采用String的序列化配置
-//        template.setKeySerializer(stringRedisSerializer);
-//        template.setHashKeySerializer(stringRedisSerializer);
-//
-//        //value和hash的value采用Json的序列化配置
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//
-//        template.afterPropertiesSet();
-//
-//        return template;
-//    }
-//}
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        //String序列号配置
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        //key和hash的key都采用String的序列化配置
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        //value和hash的value采用Json的序列化配置
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
--- redis-pool/src/test/java/cn/dhx/redispool/RedisPoolApplicationTests.java
+++ redis-pool/src/test/java/cn/dhx/redispool/RedisPoolApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.dhx.redispool;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -20,13 +24,25 @@ class RedisPoolApplicationTests {
     StringRedisTemplate stringRedisTemplate;
 
     @Test
-    void contextLoads() {
-//        Object k1 = redisTemplate.opsForValue().get("k1");
-//        System.out.println(k1);
+    void contextLoads() throws IOException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("k1","v1");
+//        map.put("k2","v2");
 
-
-        String k11 = stringRedisTemplate.opsForValue().get("k1");
+        redisTemplate.opsForValue().set("k1","v22222");
+        Object k11 = redisTemplate.opsForValue().get("k1");
         System.out.println(k11);
+        String k1 = (String) redisTemplate.opsForValue().get("k1");
+        System.out.println(k1);
+//        String s = objectMapper.readValue(k1, String.class);
+//        System.out.println(s);
+
+        String k112 = stringRedisTemplate.opsForValue().get("k1");
+        System.out.println(k112);
+        stringRedisTemplate.opsForValue().set("sk1","sv1");
+        String sk1 = stringRedisTemplate.opsForValue().get("sk1");
+        System.out.println(sk1);
     }
 
 }
