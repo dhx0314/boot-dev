2ae7988848f2f2a34ee477a3e69555965ed97adb
daihongxin <daihongxin@wilcom.com.cn>
2021-07-01 13:39:48 +0800

    redis

--- redis-pool/src/main/java/cn/dhx/redispool/Demo.java
+++ redis-pool/src/main/java/cn/dhx/redispool/Demo.java
@@ -13,7 +13,8 @@ public class Demo {
 //        HashSet<HostAndPort> set = new HashSet<>();
 //        JedisCluster jedisCluster = new JedisCluster(set);
 //        jedisCluster.close();
-        String str="172.16.2.95:7001,172.16.2.95:7002,172.16.2.95:7003,172.16.2.95:7004,172.16.2.95:7005,172.16.2.95:7006";
+//        String str="172.16.2.95:7001,172.16.2.95:7002,172.16.2.95:7003,172.16.2.95:7004,172.16.2.95:7005,172.16.2.95:7006";
+        String str="192.168.232.128:7001,192.168.232.128:7002,192.168.232.128:7003,192.168.232.128:7004,192.168.232.128:7005,192.168.232.128:7006";
         String[] split = str.split(",");
         HashSet<HostAndPort> node = new HashSet<>();
         for (String s : split) {
@@ -25,20 +26,20 @@ public class Demo {
 //        ClusterPool clusterPool = poolAutoConfiguration.initSDKPool(node);
 
         ClusterPool clusterPool = ClusterPoolConfiguration.getClusterPoolConfiguration().initSDKPool(node);
-//        for (int i = 0; i < 10; i++) {
-//            int finalI = i;
-//            new Thread(()->{
-//                JedisCluster jedisCluster=null;
-//                try {
-//                    jedisCluster = clusterPool.borrowObject();
-//                    jedisCluster.set("k"+ finalI, String.valueOf(finalI));
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }finally {
-//                    clusterPool.returnObject(jedisCluster);
-//                }
-//            }).start();
-//        }
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            new Thread(()->{
+                JedisCluster jedisCluster=null;
+                try {
+                    jedisCluster = clusterPool.borrowObject();
+                    jedisCluster.set("k"+ finalI, String.valueOf(finalI));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    clusterPool.returnObject(jedisCluster);
+                }
+            }).start();
+        }
 
         for (int i = 0; i < 10; i++) {
             int finalI = i;
--- redis-pool/src/test/java/cn/dhx/redispool/RedisPoolApplicationTests.java
+++ redis-pool/src/test/java/cn/dhx/redispool/RedisPoolApplicationTests.java
@@ -1,13 +1,32 @@
 package cn.dhx.redispool;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class RedisPoolApplicationTests {
+
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
+//        Object k1 = redisTemplate.opsForValue().get("k1");
+//        System.out.println(k1);
+
+
+        String k11 = stringRedisTemplate.opsForValue().get("k1");
+        System.out.println(k11);
     }
 
 }
