dc0426f228e6eb21e10b9c0efd8f2ee58350a1f8
dhx <329564635@qq.com>
2021-08-31 00:24:59 +0800

    mq

--- mq/src/main/java/cn/dhx/mq/rabbitmq/helloWorld/Consumer.java
+++ mq/src/main/java/cn/dhx/mq/rabbitmq/helloWorld/Consumer.java
@@ -20,6 +20,16 @@ public class Consumer {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         System.out.println("---------sonsumer");
+
+
+
+        /**
+         * 消费者消费消息
+         * 1.消费哪个队列
+         * 2.消费成功之后是否要自动应答 true 代表自动应答 false 手动应答
+         * 3.推送的消息如何进行消费的接口回调
+         * 3.消费者未成功消费的回调
+         */
         channel.basicConsume(QUEUE_NAME,true, (consumerTag,message)->{
             String s = new String(message.getBody());
             System.out.println("message-------"+s);
