46f0a98100a98451229a4df3cf2c1a5f211be446
daihongxin <daihongxin@wilcom.com.cn>
2021-11-19 15:59:54 +0800

    boot-demo

--- boot/src/main/java/cn/dhx/boot/demo/Demo10.java
+++ boot/src/main/java/cn/dhx/boot/demo/Demo10.java
@@ -9,7 +9,25 @@ public class Demo10 {
 //        System.out.println("a" + 100 + 8);//a1008
 //        System.out.println(100 + 8 + "a");//108a
 
-        Stu stu = new Stu();
-        System.out.println(stu);
+//        Stu stu = new Stu();
+//        System.out.println(stu);
+
+//        int i = 1;
+//        int a = i++;
+////        i = ++i;
+//        System.out.println(a);
+
+
+//        int a = 4;
+//        int b = 9;
+//        if (a > 1 || (b=10) > 4) {
+//            // 可以换成 | 测试
+//            System.out.println("ok300");
+//        }
+//        System.out.println("a=" + a + " b=" + b);
+//        //a=4 b=9
+
+        System.out.println( (4 < 1) ^ (6 > 3) );
+
     }
 }
