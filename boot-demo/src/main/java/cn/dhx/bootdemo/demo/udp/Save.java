a2d09b0ea5ec7f1814427e245c9f817dfd7a1f11
daihongxin <daihongxin@wilcom.com.cn>
2021-11-29 17:54:19 +0800

    udp

---
+++ boot-demo/src/main/java/cn/dhx/bootdemo/demo/udp/Save.java
@@ -0,0 +1,56 @@
+package cn.dhx.bootdemo.demo.udp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+@Component
+@Slf4j
+public class Save {
+
+    //    public static void main(String[] args) throws Exception {
+    @PostConstruct
+    public void start() {
+        new Thread(()->{
+            try {
+                fun();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+
+
+    public  void fun() throws Exception {
+
+
+        // 定义一个接收端，并且指定了接收的端口号
+//        DatagramSocket socket = new DatagramSocket(19993, InetAddress.getByName("172.16.2.155"));
+        DatagramSocket socket = new DatagramSocket(19993);
+
+        while (true) {
+            byte[] buf = new byte[1024];
+            // 解析数据包
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+            socket.receive(packet);
+
+            String ip = packet.getAddress().getHostAddress();
+
+            buf = packet.getData();
+
+            String data = new String(buf, 0, packet.getLength());
+
+//            System.out.println("收到 " + ip + " 发来的消息：" + data);
+            log.info("{} message {} ",ip,data);
+
+        }
+
+    }
+}
--- boot/src/main/java/cn/dhx/boot/demo/Demo11.java
+++ boot/src/main/java/cn/dhx/boot/demo/Demo11.java
@@ -1,15 +1,22 @@
 package cn.dhx.boot.demo;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Demo11 {
 
     public static void main(String[] args) {
         ConcurrentHashMap<String, JsonResult> hashMap = new ConcurrentHashMap<>();
-        JsonResult k = hashMap.get("k");
-        System.out.println(k);
-        if (k == null) {
-            System.out.println("------------");
+//        JsonResult k = hashMap.get("k");
+//        System.out.println(k);
+//        if (k == null) {
+//            System.out.println("------------");
+//        }
+
+        Set<Map.Entry<String, JsonResult>> entries = hashMap.entrySet();
+        for (Map.Entry<String, JsonResult> entry : entries) {
+            System.out.println(entry.getKey());
         }
     }
 }
