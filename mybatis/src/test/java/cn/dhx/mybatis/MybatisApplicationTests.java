79145d7f39043315670c1471b34481d0579d12fc
dhx <329564635@qq.com>
2022-03-07 23:04:50 +0800

    service0307

--- mybatis/src/test/java/cn/dhx/mybatis/MybatisApplicationTests.java
+++ mybatis/src/test/java/cn/dhx/mybatis/MybatisApplicationTests.java
@@ -1,9 +1,7 @@
 package cn.dhx.mybatis;
 
 
-import cn.dhx.mybatis.dao.FiledetailDao;
-import cn.dhx.mybatis.entity.Filedetail;
-import cn.dhx.mybatis.service.impl.FiledetailServiceImpl;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +13,12 @@ import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class MybatisApplicationTests {
+public class MybatisApplicationTests {
 
-    @Autowired
-    private FiledetailServiceImpl filedetailService;
 
     @Test
     public void fun1() {
-        Filedetail byId = filedetailService.getById(1);
-        System.out.println(byId.toString());
+
     }
 
 }
