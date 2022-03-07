c003475b1f24991ea5f744d6b4fbd0012278fc87
dhx <329564635@qq.com>
2022-03-07 22:49:46 +0800

    a1105

---
+++ boot/src/main/java/cn/dhx/boot/demo/Demo15.java
@@ -0,0 +1,35 @@
+package cn.dhx.boot.demo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Demo15 {
+
+    public static void main(String[] args) {
+        LocalDate now1 = LocalDate.now();
+        System.out.println(now1);//2021-12-26
+        LocalTime now2 = LocalTime.now();
+        System.out.println(now2);//14:50:24.960
+
+
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);//2021-12-26T14:50:24.960
+
+        LocalDateTime localDateTime = LocalDateTime.now().withNano(0);
+        System.out.println(localDateTime);//2021-12-26T14:50:24
+
+        LocalDate localDate = now.toLocalDate();
+        System.out.println(localDate);//2021-12-26
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = dateTimeFormatter.format(now);
+        System.out.println(format);//2021-12-26 14:57:53
+
+        String dateStr="2021-12-26 14:57:53";
+        LocalDateTime parse = LocalDateTime.parse(dateStr, dateTimeFormatter);
+        System.out.println(parse);//2021-12-26T14:57:53
+    }
+}
---
+++ boot/src/main/java/cn/dhx/boot/demo/Demo16.java
@@ -0,0 +1,23 @@
+package cn.dhx.boot.demo;
+
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Demo16 {
+
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);//2021-12-26T07:11:24.064Z
+
+        Date from = Date.from(now);
+        System.out.println(from);//Sun Dec 26 15:11:24 CST 2021
+
+        Date date = new Date();
+        System.out.println(date);//Sun Dec 26 15:11:24 CST 2021
+        Instant instant = date.toInstant();
+        System.out.println(instant);//2021-12-26T07:11:24.134Z
+    }
+}
