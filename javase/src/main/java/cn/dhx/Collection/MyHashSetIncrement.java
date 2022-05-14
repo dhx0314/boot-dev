4383cd27027becbdf3675e5020335e1c6c5c8a2b
dhx <329564635@qq.com>
2022-05-15 00:46:52 +0800

    hashset

---
+++ javase/src/main/java/cn/dhx/Collection/MyHashSetIncrement.java
@@ -0,0 +1,44 @@
+package cn.dhx.Collection;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class MyHashSetIncrement {
+
+
+    @Test
+    public void fun1() {
+        HashSet<Object> hashSet = new HashSet<>(64);
+        for (int i = 0; i < 12; i++) {
+            hashSet.add(new A(i));
+        }
+
+    }
+
+}
+
+class A{
+
+    private int i;
+
+    public A(int i) {
+        this.i = i;
+    }
+
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        A a = (A) o;
+//        return i == a.i;
+//    }
+
+    @Override
+    public int hashCode() {
+        return 15;
+    }
+}
--- javase/src/main/java/cn/dhx/demo/MyTest.java
+++ javase/src/main/java/cn/dhx/demo/MyTest.java
@@ -1,21 +1,33 @@
 package cn.dhx.demo;
 
 
+import org.junit.Test;
+
 public class MyTest {
 
     public static void main(String[] args) {
 
-        try {
-            int a = 20;
-            int b = 20;
-            float f = (float) (a + 1.2);
-            double d = b + 1.2;
-            float f2 = a + 1.2;
-            System.out.println(f2);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        int data[] = new int[3]; /*开辟了一个长度为3的数组*/
+        data[0] = 10; // 第一个元素
+        data[1] = 20; // 第二个元素
+        data[2] = 30; // 第三个元素
+        System.out.println(data.length);// 3
+
+
+        int arr[] = {1,2,3};
+        System.out.println(arr.length);	// 3
+
+        int arr2[]=new int[]{4,5,6,7};
+        System.out.println(arr2.length);// 4
+
+
+        String str[] = {"aa", "bb", "cc"};
+        char charArr[] = {'a', 'b'};
+
+        //数据类型 对象数组[][] = new 数据类型[行个数][列个数];
+        int a[][]=new int[2][3];
     }
+
 
 }
 
