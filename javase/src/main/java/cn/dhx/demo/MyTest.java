0ce011359fb3330d74d695432782f3c205e0bfda
daihongxin <daihongxin@wilcom.com.cn>
2022-05-22 00:03:26 +0800

    map

--- javase/src/main/java/cn/dhx/demo/MyTest.java
+++ javase/src/main/java/cn/dhx/demo/MyTest.java
@@ -1,23 +1,14 @@
 package cn.dhx.demo;
 
 
+
+
 public class MyTest {
 
-    public static void main(String[] args) {
-
-        try {
-            int a = 20;
-            int b = 20;
-            float f = (float) (a + 1.2);
-            double d = b + 1.2;
-            float f2 = a + 1.2;
-            System.out.println(f2);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 }
+
+
 
 
 
---
+++ javase/src/main/java/cn/dhx/map/MyHashMap.java
@@ -0,0 +1,15 @@
+package cn.dhx.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class MyHashMap {
+
+    @Test
+    public void fun1() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("01","aa");
+        hashMap.put("02","bb");
+    }
+}
