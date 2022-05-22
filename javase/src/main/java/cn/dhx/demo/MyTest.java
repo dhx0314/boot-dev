44edf8b930521b1ddfce7aa38d24d4a484e2222d
daihongxin <daihongxin@wilcom.com.cn>
2022-05-22 11:56:24 +0800

    mapaa

--- javase/src/main/java/cn/dhx/demo/MyTest.java
+++ javase/src/main/java/cn/dhx/demo/MyTest.java
@@ -1,10 +1,26 @@
 package cn.dhx.demo;
 
 
+import org.junit.Test;
 
+import java.util.HashMap;
 
 public class MyTest {
 
+    @Test
+
+    public void fun1() {
+        System.out.println("aa");
+        System.out.println("aa3223");
+        System.out.println("aa3cc");
+    }
+
+    @Test
+    public void fun2() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("01","aa");
+        hashMap.put("02","bb");
+    }
 
 }
 
