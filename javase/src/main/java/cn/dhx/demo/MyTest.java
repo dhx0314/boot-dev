faabff8bcb10c30f78a1b8d7b7d3cd4e227b526f
dhx <329564635@qq.com>
2022-05-22 11:55:17 +0800

    hash

--- javase/src/main/java/cn/dhx/demo/MyTest.java
+++ javase/src/main/java/cn/dhx/demo/MyTest.java
@@ -1,11 +1,16 @@
 package cn.dhx.demo;
 
 
-
+import org.junit.Test;
 
 public class MyTest {
 
-
+    @Test
+    public void fu1() {
+        System.out.println("--------------");
+        System.out.println("-----1---------");
+        System.out.println("-----2---------");
+    }
 }
 
 
