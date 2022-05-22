7d1b412850c975f786d23e6fb125f16c56cd4fbc
dhx <329564635@qq.com>
2022-05-22 11:07:06 +0800

    hash

---
+++ javase/src/main/java/cn/dhx/Collection/MyLinkedHashSet.java
@@ -0,0 +1,33 @@
+package cn.dhx.Collection;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class MyLinkedHashSet {
+
+
+    @Test
+    public void fun1() {
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("aa1");
+        linkedHashSet.add("aa2");
+        linkedHashSet.add("aa3");
+
+        System.out.println(linkedHashSet.toString());
+    }
+
+
+
+    @Test
+    public void fun2() {
+        HashSet<Object> hashSet = new LinkedHashSet<>(64);
+        for (int i = 0; i < 12; i++) {
+            hashSet.add(new A(i));
+        }
+
+    }
+
+}
+
---
+++ javase/src/main/java/cn/dhx/map/MyHashMap.java
@@ -0,0 +1,15 @@
+package cn.dhx.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class MyHashMap {
+
+
+    @Test
+    public void fun1() {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("aa","bb");
+    }
+}
