83b764e23f7c025782a32dd4761dd19a1c5a3dc3
dhx <329564635@qq.com>
2022-05-23 09:08:14 +0800

    hashmap test

--- javase/src/main/java/cn/dhx/map/MyHashMap.java
+++ javase/src/main/java/cn/dhx/map/MyHashMap.java
@@ -3,13 +3,97 @@ package cn.dhx.map;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyHashMap {
 
     @Test
     public void fun1() {
         HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("01","aa");
-        hashMap.put("02","bb");
+        hashMap.put("01", "aa");
+        hashMap.put("02", "bb");
+        System.out.println(hashMap.get("")); //null
+        System.out.println(hashMap.get(null)); // null
+
+        int size = hashMap.size();
+        System.out.println(size);
+
+    }
+
+
+
+
+
+    @Test
+    public void fun1a() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("01", "aa");
+        hashMap.put("02", "bb");
+        hashMap.put("03", "cc");
+
+        Set<Map.Entry<String, String>> entries = hashMap.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            System.out.println(entry.getKey() + "--" + entry.getValue());
+        }
+
+        hashMap.forEach((k, v) -> {
+            System.out.println(k + "--" + v);
+        });
+
+
+        Set<String> keys = hashMap.keySet();
+        for (String key : keys) {
+            String value = hashMap.get(key);
+            System.out.println(key + "--" + value);
+        }
+    }
+
+    @Test
+    public void fun2() {
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("01", "aa");
+        concurrentHashMap.put("02", "bb");
+        System.out.println(concurrentHashMap.get(""));  //null
+        System.out.println(concurrentHashMap.get(null)); //java.lang.NullPointerException
+
+
+    }
+
+
+    @Test
+    public void fun3() {
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        for (int i = 0; i < 6; i++) {
+            hashMap.put(new A(i),new Object());
+        }
+        for (int i = 0; i < 10; i++) {
+            hashMap.put(String.valueOf(i),new Object());
+        }
+    }
+}
+
+
+class A{
+
+    private int i;
+
+    public A(int i) {
+        this.i = i;
+    }
+
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        A a = (A) o;
+//        return i == a.i;
+//    }
+
+    @Override
+    public int hashCode() {
+        return 4;
     }
 }
