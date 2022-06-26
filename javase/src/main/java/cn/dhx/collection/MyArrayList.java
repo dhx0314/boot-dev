7192ec5162a875ed4e26a63245805aa2e074d15f
dhx <329564635@qq.com>
2022-06-26 09:32:02 +0800

    jdk8

--- javase/src/main/java/cn/dhx/collection/MyArrayList.java
+++ javase/src/main/java/cn/dhx/collection/MyArrayList.java
@@ -3,6 +3,8 @@ package cn.dhx.collection;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyArrayList {
 
@@ -19,5 +21,37 @@ public class MyArrayList {
 
         strings.addAll(strings1);
         System.out.println(strings.size());
+    }
+
+    @Test
+    public void fun2() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        Object[] objects = arrayList.toArray();
+        System.out.println(Arrays.toString(objects));
+    }
+
+    @Test
+    public void fun3() {
+        String [] list = new String[]{"1","2"};
+        List<String> strings = Arrays.asList(list);
+        strings.add("333");
+        //java.lang.UnsupportedOperationException
+
+
+//        public static <T> List<T> asList(T... a) {
+//            return new Arrays.ArrayList<>(a);
+//        }
+    }
+
+    @Test
+    public void fun4() {
+        String [] list = new String[]{"1","2"};
+        List<String> strings = Arrays.asList(list);
+        ArrayList<String> arrayList = new ArrayList<>(strings);
+        arrayList.add("333");
+        System.out.println(arrayList);
     }
 }
