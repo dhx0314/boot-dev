3dcd0c189cd3df1dbc448a7404dc6d43548a553e
daihongxin <daihongxin@wilcom.com.cn>
2022-07-16 14:14:17 +0800

    test0716

--- mybatis-plus/src/test/java/cn/dhx/mybatisplus/MybatisPlusApplicationTests.java
+++ mybatis-plus/src/test/java/cn/dhx/mybatisplus/MybatisPlusApplicationTests.java
@@ -1,12 +1,9 @@
 package cn.dhx.mybatisplus;
 
 
-import cn.dhx.mybatisplus.dao.FiledetailDao;
 
-
-import cn.dhx.mybatisplus.entity.Filedetail;
-import cn.dhx.mybatisplus.entity.LeftDto2;
-import cn.dhx.mybatisplus.service.impl.FiledetailServiceImpl;
+import cn.dhx.mybatisplus.entity.TSkillDetail;
+import cn.dhx.mybatisplus.service.impl.TSkillDetailServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,56 +19,16 @@ class MybatisPlusApplicationTests {
 
 
     @Autowired
-    FiledetailServiceImpl filedetailService;
-
-    @Autowired
-    FiledetailDao filedetailDao;
+    private TSkillDetailServiceImpl tSkillDetailService;
 
     @Test
-    public void ffun1() {
-
-//        List<LeftDto> leftDtos = filedetailDao.selectAllLeft();
-//
-//        System.out.println(leftDtos);
-//        Filedetail filedetail = new Filedetail();
-//        filedetail.setAni("1234");
-//        filedetail.setDnis("3789");
-//        boolean save = filedetailService.save(filedetail);
-//        System.out.println(save);
-//
-//        Filedetail byId = filedetailService.getById(10);
+    public void fun1() {
+//        TSkillDetail byId = tSkillDetailService.getById(1);
 //        System.out.println(byId);
 
-  //      LambdaQueryWrapperBuilder<Object> objectLambdaQueryWrapperBuilder = LambdaQueryWrapperBuilder.newBuilder();
-    //    LambdaQueryWrapperBuilder<Filedetail> filedetailLambdaQueryWrapperBuilder = LambdaQueryWrapperBuilder.<Filedetail>newBuilder();
-
-
-        List<LeftDto2> leftDao2s = filedetailDao.selectAllLeft2();
-        System.out.println(leftDao2s);
-        for (LeftDto2 leftDao2 : leftDao2s) {
-            System.out.println(leftDao2);
-        }
-
+        tSkillDetailService.fun1();
     }
 
-
-//    public class GeneralMethod {
-//        public static <U> void print(U[] list) {
-//            System.out.println();
-//            for (int i = 0; i < list.length; i++) {
-//                System.out.print(" " + list[i]);
-//            }
-//            System.out.println();
-//        }
-//        public static void main(String[] args) {
-//            String a[]={"a","b","c","d","e"};
-//            Character b[]={'1','2','3','4','5'};
-//            Integer c[]={1,2,3,4,5};
-//            GeneralMethod.print(a);
-//            GeneralMethod.print(b);
-//            GeneralMethod.print(c);
-//        }
-//    }
 
 
 
