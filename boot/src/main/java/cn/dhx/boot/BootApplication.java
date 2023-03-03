50fa9d3e7f46a0eac1581a0795f9b5c165d93c94
daihongxin <daihongxin@wilcom.com.cn>
2023-03-03 17:28:46 +0800

    增加boot

---
+++ boot/src/main/java/cn/dhx/boot/BootApplication.java
@@ -0,0 +1,19 @@
+package cn.dhx.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Author daihongxin
+ * @create 2023/3/3 17:00
+ */
+@SpringBootApplication
+@EnableScheduling
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
