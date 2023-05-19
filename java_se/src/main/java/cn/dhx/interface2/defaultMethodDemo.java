9a79d918236a6385af5271f6d60f70b7ce2a6387
daihongxin <daihongxin@wilcom.com.cn>
2023-05-20 00:55:10 +0800

    interface

--- java_se/src/main/java/cn/dhx/interface2/defaultMethodDemo.java
+++ java_se/src/main/java/cn/dhx/interface2/defaultMethodDemo.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 /**
  * @Author daihongxin
  * @create 2023/5/15 15:26
+ *
+ * 接口默认方法重写,实现类可以重写这个方法，
+ * 接口可以有静态方法
  */
 @Slf4j
 public class defaultMethodDemo {
@@ -22,6 +25,9 @@ interface People {
 
     default public String getName() {
         return "people";
+    }
+    static public String getPhone() {
+        return "1234";
     }
 }
 
